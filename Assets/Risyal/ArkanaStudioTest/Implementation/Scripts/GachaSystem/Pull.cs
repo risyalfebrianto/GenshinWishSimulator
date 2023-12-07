2880bf24291bca6cd269a9d1ac8207a3ad1a6e3f
Rizky Purnama <74520252+risyalfebrianto@users.noreply.github.com>
2023-12-07 07:43:13 +0700

    Add 50:50

--- Assets/Risyal/ArkanaStudioTest/Implementation/Scripts/GachaSystem/Pull.cs
+++ Assets/Risyal/ArkanaStudioTest/Implementation/Scripts/GachaSystem/Pull.cs
@@ -175,6 +175,27 @@ namespace Assets.Risyal.ArkanaStudioTest.Implementation.Scripts.MainGamePlay
                 currentSuperRarePity--;
                 currentRarePity--;
 
+                if (currentSuperRarePity == _pityData.DefaultSuperRarePity / 2)
+                {
+                    var random = Random.Range(0, 2);
+                    var get = random == 0;
+
+                    if (get)
+                    {
+                        if (Pulltype == Pulltype.Character)
+                        {
+                            characterData = GetCharacterSuperRare();
+                        }
+                        else
+                        {
+                            weaponData = GetWeaponSuperRare();
+
+                            getWeapon = true;
+                        }
+
+                        currentSuperRarePity = _pityData.DefaultSuperRarePity;
+                    }
+                }
                 if (currentSuperRarePity <= 0)
                 {
                     if (Pulltype == Pulltype.Character)
